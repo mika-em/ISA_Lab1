b25e7ff917526d8f71d41b1f2bfdfa3a4b6d99ca
mikaem <mkmanaligod@gmail.com>
2024-09-12 15:46:09 -0700

    revert change

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ class Game {
         this.colorOptions = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#8B00FF'];
         this.userOrder = [];
         this.userOrderIndex = 0;
-        window.addEventListener("resize", () => this.onWindowResize());
+        window.addEventListener("windowResize", () => this.onWindowResize());
     }
 
     createButtons() {
